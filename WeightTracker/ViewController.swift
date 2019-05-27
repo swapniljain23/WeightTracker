7431d813dead50bed02bf220f0d3e24883208d87
Swapnil Jain <swapniljain@nehas-macbook-pro.local>
2019-05-26 22:00:08 -0700

    Initial Commit

---
+++ WeightTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightTracker
+//
+//  Created by Swapnil Jain on 5/26/19.
+//  Copyright © 2019 Swapnil Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
